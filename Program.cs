bd2e1b6c24e2f0efb4049f95e41c60b00ae6185a
Ryan <zjkung1123@fugle.tw>
2021-12-23 18:58:00 +0800

    1. init

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Concurrent;
+using System.Threading.Channels;
+var htmlCollection = Channel.CreateBounded<string>(3);
+
+var urls = new ConcurrentQueue<string>(new[]{
+    "https://www.google.com",
+    "https://www.bing.com",
+    "https://www.yahoo.com",
+});
+
+using HttpClient client = new();
+await Task.Factory.StartNew(async () =>
+{
+    var tasks = urls.Select(async url =>
+    {
+        var html = await (await client.GetAsync(url)).Content.ReadAsStringAsync();
+        await htmlCollection.Writer.WriteAsync(html);
+    });
+    await Task.WhenAll(tasks);
+    htmlCollection.Writer.Complete();
+});
+
+
+while (await htmlCollection.Reader.WaitToReadAsync())
+{
+    if (htmlCollection.Reader.TryRead(out var message))
+    {
+        Console.WriteLine(message);
+    }
+}
